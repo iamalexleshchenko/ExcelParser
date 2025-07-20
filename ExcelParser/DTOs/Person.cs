25e02186bf820b0b9d5127b12ec62c43f11706db
alexanderleshchenko <iamalexleshchenko@gmail.com>
2025-07-20 13:40:27 +0200

    Реализовал контроллер и примитивный парсер excel-файла

---
+++ ExcelParser/DTOs/Person.cs
@@ -0,0 +1,8 @@
+namespace ExcelParser.DTOs;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string MiddleName { get; set; }
+}
